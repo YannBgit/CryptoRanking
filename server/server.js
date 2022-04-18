48ae3ba3b7ab39beb9ab8e118c5fed318a31ebae
dytq <user@debian-BULLSEYE-live-builder-AMD64>
2022-04-18 20:38:05 +0200

    ajout de la base de données

--- server/server.js
+++ server/server.js
@@ -1,23 +1,56 @@
 const express = require('express')
-const app = express()
-
+const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
+const app = express()
+ 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-mongoose.connect('mongodb://localhost/cryptotracker')
+// connection à la base de données
+mongoose.connect('mongodb://localhost/cryptotracker', {
+}).then(() => {
+	console.log("mongo db connected");
+}).catch(err => {
+	console.log("failed to connect to the database",err);
+});
+
+
+const userSchema = new mongoose.Schema ( {
+	email : {type : String, required : true},
+	password : {type : String, required : true}
+})
+const User = mongoose.model('User', userSchema);
 
 app.get("/test", (req, res) => {
 	res.json({"users": ["userOne","userTwo","userTree"]})
 })
 
 app.post("/api/connexion", async (req, res) => {
-	const record = req.body
-	console.log("connexion")
+	const data = req.body
+	console.log(data.email)
+	console.log(data.password)
+	
 	res.redirect("/")
 } ) 
 
 app.post("/api/inscription", async (req, res) => {
-	const record = req.body
-	console.log("inscription")
+	const data = req.body
+	console.log(data.email)
+	console.log(data.password[0])
+	if(data.password[0] == data.password[1]) {
+		const user = new User ( {
+			email : data.email,
+			password : data.password[0]
+		})
+		
+		user.save().then(user => {
+			console.log(user)
+		}).catch (err => {
+			console.log(err)
+		});
+	} else {
+		console.log("error: password not match");
+	}
 	res.redirect("/")
 } ) 
 
