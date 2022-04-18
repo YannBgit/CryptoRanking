20938b70a88daa3eccca2e608b2d9392f5031902
dytq <user@debian-BULLSEYE-live-builder-AMD64>
2022-04-18 18:24:19 +0200

    add mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+
 app.get("/test", (req, res) => {
 	res.json({"users": ["userOne","userTwo","userTree"]})
 })
